02705d7b4e2b312df80c38787d7157f2d2a9cbb6
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-03 18:23:21 +0700

    feat: adding fetcher server action

---
+++ src/action/fetcher.ts
@@ -0,0 +1,14 @@
+"use server";
+
+export type BaseResponse<T> = {
+  code: number;
+  message: string;
+  data?: T;
+};
+
+export async function fetcher<T = unknown>(path: string, init?: RequestInit) {
+  const basePath = process.env.BE_BASE_URL;
+  const request = await fetch(`${basePath}${path}`, init);
+  const response = (await request.json()) as BaseResponse<T>;
+  return response;
+}
