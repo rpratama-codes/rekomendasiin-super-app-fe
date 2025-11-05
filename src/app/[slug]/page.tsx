79b8abea17841320522cbc8d5ae5edf886f9778a
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-05 15:17:49 +0700

    feat: recommendation page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import { fetcher } from "@/action/fetcher";
+import Footer from "@/component/base/footer";
+import NavBar from "@/component/base/navbar";
+import Section from "@/component/base/section";
+import Tagline from "@/component/specific/tagline";
+import RecomendationForm from "./content";
+
+export type Criteria = { id: string; criteria_name: string }[];
+
+export default async function Page() {
+  const criteria = await fetcher<Criteria>("/store-front/list-criteria");
+
+  return (
+    <div className="flex flex-col justify-between w-full h-screen">
+      <NavBar />
+      <Section>
+        <RecomendationForm criteria={criteria.data} />
+      </Section>
+      <Tagline />
+      <Footer />
+    </div>
+  );
+}
