41de22280fa22ea133301849bb17ff85ee8c29a3
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-05 15:17:02 +0700

    fix: add link to app logo

--- src/component/base/navbar.tsx
+++ src/component/base/navbar.tsx
@@ -1,21 +1,26 @@
 /** biome-ignore-all lint/performance/noImgElement: <No Need Next Image Optimization> */
+
+import Link from "next/link";
+
 export default function NavBar() {
   return (
-    <div className="flex w-full h-14 justify-center items-center bg-gradient-to-bl from-red-600 to-red-800 text-white">
+    <div className="flex w-full min-h-14 justify-center items-center bg-linear-to-bl from-red-600 to-red-800 text-white">
       <div className="container">
         <div
           id="nav-body"
           className="flex w-full h-full justify-between items-center"
         >
-          <div id="nav-left" className="flex gap-2 items-center">
-            <div className="w-10 h-10">
-              <img
-                className="rounded-md"
-                src="https://rekomendasiin.rpratama.web.id/img/logo.svg"
-                alt="logo-rekomendasiin"
-              />
-            </div>
-            <p>Rekomendasiin</p>
+          <div id="nav-left" className="flex gap-2">
+            <Link className="flex gap-2 items-center" href={"/"}>
+              <div className="w-10 h-10">
+                <img
+                  className="rounded-md"
+                  src="https://rekomendasiin.rpratama.web.id/img/logo.svg"
+                  alt="logo-rekomendasiin"
+                />
+              </div>
+              <p>Rekomendasiin</p>
+            </Link>
           </div>
 
           <div id="nav-right">
