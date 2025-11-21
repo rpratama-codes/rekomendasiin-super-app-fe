a12e8db3d4883bd105f62dce77b026788891c610
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-21 16:13:17 +0700

    fix: remove deprecated middleware to new nextjs proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,17 @@
 import { type NextRequest, NextResponse } from "next/server";
-import { checkSession } from "./action/fetcher2";
+import { refreshToken } from "./action/refresh-token";
 
 export default async function proxy(req: NextRequest) {
+  /**
+   * It's not necessary to check auth via proxy.
+   * But i keep for later!.
+   */
   const pathname = req.nextUrl.pathname;
+  const dashboardRoute = pathname.startsWith("/dashboard");
+  const authRoute = pathname.startsWith("/dashboard");
 
-  if (pathname.startsWith("/dashboard")) {
-    const session = await checkSession();
+  if (dashboardRoute || authRoute) {
+    const session = await refreshToken();
 
     if (!session) {
       return NextResponse.redirect(new URL(`/api/auth/signin`, req.url));
@@ -16,5 +22,5 @@ export default async function proxy(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/auth/:path*"],
 };
