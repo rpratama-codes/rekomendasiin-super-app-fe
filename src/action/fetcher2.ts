61bcbbd4235929fe50dbb62dd0e850c7cea932dd
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-21 16:14:01 +0700

    fix: fixing fetcher2

--- src/action/fetcher2.ts
+++ src/action/fetcher2.ts
@@ -1,47 +1,6 @@
 "use server";
 
-import * as jose from "jose";
-import { auth, backendAuth, unstable_update } from "@/app/auth";
-
-export const checkSession = async () => {
-  const session = await auth();
-
-  if (!session) {
-    return null;
-  }
-
-  const accessPayload = jose.decodeJwt(session.access_token as string);
-  const currentDate = Math.floor(Date.now() / 1000);
-  const expiredDate = Number(accessPayload.exp);
-  const isExp = currentDate > expiredDate;
-  console.log("Is Expired : ", isExp);
-
-  if (isExp) {
-    const refresh = await backendAuth({
-      type: "backend-refresh",
-      token: session.refresh_token as string,
-    });
-
-    if (!refresh) {
-      return null;
-    }
-
-    const newTokenPayload = jose.decodeJwt(refresh.refresh_token);
-    const newSession = await unstable_update({
-      ...refresh.user,
-      ...newTokenPayload,
-      access_token: refresh.access_token,
-      refresh_token: refresh.refresh_token,
-      exp: Number(newTokenPayload.exp),
-    });
-
-    console.log("CurrentSession", newSession);
-    return newSession;
-  }
-
-  console.log("CurrentSession", session);
-  return session;
-};
+import type { Session } from "next-auth";
 
 export type Fetcher2BaseResponse<T> = {
   code: number;
@@ -49,17 +8,27 @@ export type Fetcher2BaseResponse<T> = {
   data?: T;
 };
 
-export async function Fetcher2<T = unknown>(path: string, init?: RequestInit) {
-  const session = await checkSession();
-  const { headers: userHeaders, ...restInit } = init || {};
+export async function Fetcher2<T = unknown>({
+  path,
+  session,
+  options,
+}: {
+  path: string;
+  session?: Session;
+  options?: RequestInit;
+}) {
+  const { headers: userHeaders, ...restOptions } = options || {};
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
-  headers.append("Authorization", `Bearer ${session?.access_token as string}`);
+
+  if (session?.access_token) {
+    headers.append("Authorization", `Bearer ${session?.access_token}`);
+  }
 
   const baseUrl = process.env.BE_BASE_URL;
   const request = await fetch(`${baseUrl}${path}`, {
     headers: { ...headers, ...userHeaders },
-    ...restInit,
+    ...restOptions,
   });
 
   const response = (await request.json()) as Fetcher2BaseResponse<T>;
