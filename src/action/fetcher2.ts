228a1159cd1275b48b291f487a35546f3716323a
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-11-22 21:20:33 +0700

    fix: adjust fetcher2

--- src/action/fetcher2.ts
+++ src/action/fetcher2.ts
@@ -1,33 +1,28 @@
 "use server";
 
-import type { Session } from "next-auth";
-
 export type Fetcher2BaseResponse<T> = {
   code: number;
   message: string;
   data?: T;
 };
 
-export async function Fetcher2<T = unknown>({
+export async function fetcher2<T = unknown>({
   path,
   session,
   options,
 }: {
   path: string;
-  session?: Session;
+  session?: { access_token?: string };
   options?: RequestInit;
 }) {
   const { headers: userHeaders, ...restOptions } = options || {};
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
-  if (session?.access_token) {
-    headers.append("Authorization", `Bearer ${session?.access_token}`);
-  }
 
   const baseUrl = process.env.BE_BASE_URL;
   const request = await fetch(`${baseUrl}${path}`, {
-    headers: { ...headers, ...userHeaders },
+    headers: {
+      ...(session ? { Authorization: `Bearer ${session?.access_token}` } : {}),
+      ...(userHeaders ?? {}),
+    },
     ...restOptions,
   });
 
